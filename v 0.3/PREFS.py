125f70f02a4d49bcb17dec9f11d5d5fdc446d12f
Chkoupinator <the.chkoupi@gmail.com>
2017-12-30 02:59:33 +0100

    Create PREFS.py

---
+++ v 0.3/PREFS.py
@@ -0,0 +1,6 @@
+prefix = '!'
+
+title = "Chkoupinabot, the highly unstable but functioning bot"
+
+description = "A bot made by **Chkoupinator#1064** for personal usage and needs. Has a profile system and some fun "\
+              "commands and lotsa features to come!"
